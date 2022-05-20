629817a191023b1a9fc09635686ead66e7e9169e
Esteban G <tapitarias@gmail.com>
2022-05-20 08:29:19 -0500

    Observer pattern

--- Creationals/Prototype/index.ts
+++ Creationals/Prototype/index.ts
@@ -1,27 +1,26 @@
-interface IPrototype {
-  clone(): IPrototype;
+interface Prototype {
+  clone(): Prototype;
 }
 
-class Perro implements IPrototype {
-  private name: string;
-  private age: number;
-  private color: string;
+interface Animal {
+  name: string;
+  color: string;
+}
 
-  constructor(name: string, age: number, color: string) {
+class Cat implements Prototype {
+  private name: string;
+  private color: string;
+  constructor(name: string, color: string) {
     this.name = name;
-    this.age = age;
     this.color = color;
   }
-
-  clone(): Perro {
-    return new Perro(this.name, this.age, this.color);
+  clone(): Prototype {
+    return new Cat(this.name, this.color);
   }
 }
 
-const maku = new Perro("Makú", 2, "White");
-const makuDuplicated = maku.clone();
-
-console.log(maku);
-console.log(makuDuplicated);
-console.log(maku == makuDuplicated);
-console.log(maku === makuDuplicated);
+// CLIENT CODE
+const trueno = new Cat("Trueno", "Gray");
+console.log(trueno);
+const trueno2 = trueno.clone();
+console.log(trueno2);
